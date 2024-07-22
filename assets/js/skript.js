2a17a21462de514c22a6e8bd21edc988bd8a0507
Florian Hass <f91hama@gmail.com>
2024-07-22 10:58:34 +0000

    JS add basic html structure and script initialization

---
+++ assets/js/skript.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+    let playerWins = 0;
+    let computerWins = 0;
+  });
