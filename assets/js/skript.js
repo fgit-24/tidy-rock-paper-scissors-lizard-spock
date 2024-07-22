e03c264971124824c8124b7f0f6d31cd58c32286
Florian Hass <f91hama@gmail.com>
2024-07-22 11:07:39 +0000

    JS add dom elements setup

--- assets/js/skript.js
+++ assets/js/skript.js
@@ -3,3 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
     let playerWins = 0;
     let computerWins = 0;
   });
+
+  const playerDisplay = document.getElementById("playerDisplay");
+  const computerDisplay = document.getElementById("computerDisplay");
+  const resultDisplay = document.getElementById("resultDisplay");
+  const totalPlayerResult = document.getElementById("totalPlayerResult");
+  const totalComputerResult = document.getElementById("totalComputerResult");
+  const buttons = document.querySelectorAll("#gameButtonContainer button");
+  const newGame = document.getElementById("newGame");
